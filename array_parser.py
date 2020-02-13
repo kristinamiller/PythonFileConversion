10133225637bc39f52f4c6c1df17b0dfc1182921
kristinamiller <kfm287@gmail.com>
2020-02-13 13:08:16 -0800

    hcd conditional is working

--- array_parser.py
+++ array_parser.py
@@ -23,45 +23,56 @@ unidec_dict = {
 }
 
 
-def parse_input(inputfolder):
+def parse_input(inputfolder, conditionals):
     file_list = (os.listdir(inputfolder))
     os.chdir(inputfolder)
 
     for filename in file_list:
         if is_valid_file(filename):
-            read_file(filename)
+            read_file(filename, conditionals)
     # raise RuntimeError('this is an error because I say so')
 
 
 def is_valid_file(filename):
-    if filename.find('easy_file_modified_for_testing') > -1:
+    if filename.find('2-10-test-file') > -1:
+    # if filename.find('single_MSlevel_multiplescans_testfile_2') > -1:
+    # if filename.find('easy_file_modified_for_testing') > -1:
     # if filename.find('easiest_file_single_scan_testfile-1') > -1:
         return True
     else:
         return False
 
 
-def read_file(filename):
+def read_file(filename, conditionals):
     output = []
     unidec_rows = []
     metadata_row = []
-    unidec_csv = create_new_csv(unidec_dict.keys(), 'unidec')
-    metadata_csv = create_new_csv(
-        metadata_columns, 'metadata')
+    # unidec_csv = create_new_csv(unidec_dict.keys(), 'unidec')
+    # metadata_csv = create_new_csv(
+    #     metadata_columns, 'metadata')
+    cur_hcd = 0
 
     with open(filename, 'r') as rf:
         lines = rf.readlines()
         i = 0
         mid_scan = False
+        first_scan = True
         while i < len(lines):
-            if not mid_scan:
-                new_scan_index = lines[i].find(unidec_dict['scan'])
-                if new_scan_index > -1:
+            if not mid_scan: 
+                new_scan_index = lines[i].find(unidec_dict['scan']) 
+                if new_scan_index > -1: #we know we've hit a new scan
                     mid_scan = True
                     scan_number = lines[i][new_scan_index +
                                            len(unidec_dict['scan']):].strip()
                     metadata_row = write_metadata_row(
-                        scan_number, lines[i:i+143])
+                        scan_number, lines[i:i+143]) #this is bad here. not precise. 
+                    if first_scan or (conditionals['hcd'] == 'true' and cur_hcd != metadata_row['HCD energy']):
+                        unidec_csv = create_new_csv('unidec',
+                                                    unidec_dict.keys(), scan_number, metadata_row['HCD energy'], metadata_row['MS Level'])
+                        metadata_csv = create_new_csv('metadata',
+                                                      metadata_columns, scan_number, metadata_row['HCD energy'], metadata_row['MS Level'])
+                        first_scan = False
+                    cur_hcd = metadata_row['HCD energy']
                     # append_rows(metadata_csv, [metadata_row])
                     with open(metadata_csv, 'a') as metadata_csv_file:
                         csv_writer = csv.writer(metadata_csv_file, delimiter=',')
@@ -96,7 +107,7 @@ def write_unidec_rows(scan_number, mz_array, intensity_array):
 
 def write_metadata_row(scan_number, lines):
     output_dict = {'scan number': scan_number}
-
+    #once we find the item for a column it breaks out of the inner loop and starts looking for the next one.
     for column_name, search_inst in metadata_dict.items():
         i = 0
         while i < len(lines):
@@ -106,6 +117,7 @@ def write_metadata_row(scan_number, lines):
                 output_value = lines[i][start_idx:].strip()
                 output_value = output_value.split(search_inst[2])[0]
                 output_dict[column_name] = output_value
+                break
             i += 1
 
     return(output_dict)
@@ -117,8 +129,11 @@ def append_rows(csv, rows):
             csv_writer.writerow(row)
 
 
-def create_new_csv(column_names, type):
-    outputCSV = '12' + type + str(round(cur_time)) + '.csv'
+def create_new_csv(type, column_names, scan_number, hcd_energy, ms_level):
+    title_elements = ['07', scan_number,
+                      hcd_energy, ms_level, type, str(round(cur_time)), '.csv']
+    s = '_'
+    outputCSV = s.join(title_elements)
 
     with open(outputCSV, 'w') as new_csv:
         csv_writer = csv.writer(new_csv, delimiter=',')
@@ -131,9 +146,12 @@ def main():
     # inputfolder = '/Users/kristinamiller/Documents/Freelancing/Genentech/first-project/test-read-folder'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--folder', type=str, default='/Users/kristinamiller/Documents/Freelancing/Genentech/first-project/test-read-folder', help='the folder to select files from')
+    parser.add_argument('--folder', type=str, default='/Users/kristinamiller/Documents/Freelancing/Genentech/first-project/2-13-testing', help='the folder to select files from')
+    parser.add_argument('--hcd', type=str, default='true',
+                        help='True or False to export to new file when HCD value changes')
     args = parser.parse_args()
-    parse_input(args.folder)
+    conditionals = {'hcd': args.hcd}
+    parse_input(args.folder, conditionals)
     
 
 if __name__ == '__main__':
---
+++ loop_tester.py
@@ -0,0 +1,8 @@
+for i in range(10):
+  j = 0
+  while j < 3:
+    if j == 2:
+      break
+    print(j, end=', ')
+
+    j+=1
